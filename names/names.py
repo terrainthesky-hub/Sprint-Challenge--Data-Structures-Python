d3111b3cbd021dad73b1e9f4ba4428cbf7c22815
Lesley Rich <lesley.t.rich@gmail.com>
2020-08-14 16:25:31 -0700

    fixed_two

--- names/names.py
+++ names/names.py
@@ -1,5 +1,65 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None     
+
+    def insert(self, value):
+        # check if the value is less than the current node's value 
+        if value < self.value:
+            # does the current node have a left child?
+            if self.left:
+                self.left.insert(value)
+            # otherwise, it doesn't have a left child
+            # we can park the new node here 
+            else:
+                self.left = BSTNode(value)
+        # otherwise the value is greater or equal to the current node's value 
+        else:
+            # does the current node have a right child?
+            if self.right:
+                # if it does, call the right child's `insert` method to repeat the process
+                self.right.insert(value)
+            # otherwise, it doesn't have a right child 
+            # we can park the new node here 
+            else:
+                self.right = BSTNode(value)
+
+    def dupes(self): 
+        seen = []
+
+        
+        if self.left:
+            self.left.dupes()
+        if self.right:
+            self.right.dupes()
+        seen.append(self.value)
+        if self.value in seen:
+            duplicates.append(self.value)
+        # global previous 
+    
+        # # If root is null then return 
+        # if self is None: 
+        #     return
+    
+        # dupes(self.left) 
+    
+        # if previous is not None: 
+    
+            
+        #     if self.value == previous.value: 
+        #         duplicates.append(self.value)
+    
+   
+        # previous = self 
+        # dupes(self.right) 
+
+
+
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -20,12 +80,21 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-
-#This is O(nlog)
+bst = BSTNode(names_1[0])
 
 for x in names_1:
-    if x in names_2:
-        duplicates.append(x)
+    bst.insert(x)
+for x in names_2:
+    bst.insert(x)
+bst.dupes()
+
+
+#This is O(n^2log)
+
+# for x in names_1:
+#     if x in names_2:
+#         duplicates.append(x)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,37 +1,16 @@
 class RingBuffer:
-    def __init__(self, capacity, data = []):
-        self.index = 0
+    def __init__(self, capacity):
+        self.storage = []
         self.capacity = capacity
-        self._data = list(data)[-capacity:]
+        self.index = 0
 
-    def append(self, value):
-        
-        if len(self._data) == self.capacity:
-            self._data[self.index] = value
+    def append(self, item):      
+        if len(self.storage) == self.capacity:
+            self.storage[self.index] = item
         else:
-            self._data.append(value)
+            self.storage.append(item)
         self.index = (self.index + 1) % self.capacity
 
     def get(self):
-        return self._data
-
-
-
-
-
-# class RingBuffer:
-#     def __init__(self, capacity):
-#         self.storage = []
-#         self.capacity = capacity
-#         self.oldest = []
-
-#     def append(self, item):
-#         if len(self.storage) < 2:
-#             return self.storage.append(item), self.oldest.insert(0, item)
-            
-#         if len(self.storage) >= 2:
-#                 return self.storage.insert(0, item), self.storage.remove(self.oldest[2]), self.oldest.pop()
-
-#     def get(self):
-#         return self.storage
+        return self.storage
 
